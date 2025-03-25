be17462201591c77fbfd0e35defc2134288d89e1
Alvaro Domínguez <alvaro.fabricio_20@outlook.com>
2025-03-25 19:08:05 -0300

    footer hecho y otras funcionalidades

---
+++ javascript.js
@@ -0,0 +1,9 @@
+
+
+document.getElementById('backToTop').addEventListener('click', function(e) {
+    e.preventDefault();
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
