01fd10c40f713c4a8eeed9e4ff400e0a30d02abe
Alvaro Domínguez <alvaro.fabricio_20@outlook.com>
2025-03-26 21:33:32 -0300

    hecha la página de contacto y el servicio de mail está funcionando, portafolios ya funcional

--- javascript.js
+++ javascript.js
@@ -1,9 +1,19 @@
 
-
-document.getElementById('backToTop').addEventListener('click', function(e) {
+document.getElementById('backToTop')?.addEventListener('click', (e) => {
     e.preventDefault();
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    });
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  });
+
+function scrollToElement(elementSelector, instance = 0) {
+    const elements = document.querySelectorAll(elementSelector);
+    if (elements.length > instance) {
+        elements[instance].scrollIntoView({ behavior: 'smooth' });
+    }
+}
+
+const link3 = document.getElementById("link3");
+
+link3.addEventListener('click', (e) => {
+    e.preventDefault(); // Añade esto para evitar el "saltito" del href
+    scrollToElement('.skills-section'); // Usa el selector de tu sección de habilidades
 });
